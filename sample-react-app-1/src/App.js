37bb4777a3354efdd7ceb6aee9eea0d620350731
kudumens Richard <support@rhomicom.com>
2023-08-29 17:32:37 +0000

    Test process.env

--- sample-react-app-1/src/App.js
+++ sample-react-app-1/src/App.js
@@ -3,12 +3,13 @@ import Header from './Components/Header/Header';
 import conceptsData from './Data/ConceptsData';
 
 function App() {
+
   return (
     <div>
       <Header/>
       <ul id="concepts">
         {
-        conceptsData.map((itm)=> (<Concept title={itm.title} image={itm.image} description={itm.description}/>))
+        conceptsData.map((itm, i)=> (<Concept key={`${i}-${Math.random()}`} title={itm.title} image={itm.image} description={itm.description}/>))
         }
       </ul>
     </div>
--- sample-react-app-1/src/index.js
+++ sample-react-app-1/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-
+console.log(process.env.NODE_VERSION);
+console.log(process.env.TACMS_TEST_ENV_CLIENT_ID);
+console.log(process.env.NODE_ENV);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
